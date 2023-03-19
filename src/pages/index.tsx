0eebcec9f7637871fc9f4e263d33d5d2a014735e
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-03-19 19:52:28 +0800

    style: updated weights

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,17 @@ import { Kumbh_Sans, Roboto_Slab, Space_Mono } from "next/font/google";
 
 const KumbhSans = Kumbh_Sans({
   subsets: ["latin"],
-  weight: ['700'],
-  variable: '--font-kumbh-sans'
+  weight: ["700"],
+  variable: "--font-kumbh-sans",
 });
 const RobotoSlab = Roboto_Slab({
   subsets: ["latin"],
-  weight: ['700'],
-  variable: '--font-roboto-slab'
+  weight: ["700"],
+  variable: "--font-roboto-slab",
 });
 const SpaceMono = Space_Mono({
-  weight: ['700'],
-  variable: '--font-space-mono',
+  weight: ["700"],
+  variable: "--font-space-mono",
   preload: false,
 });
 
