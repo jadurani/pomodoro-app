8fbae060c44fb6f4028029dbc785c6c4e702f6e9
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-03-19 20:58:40 +0800

    style: added font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,53 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   // Ensure these match with .storybook/preview.js
   theme: {
+    fontSize: {
+      h1: [
+        "100px",
+        {
+          lineHeight: "120px",
+          letterSpacing: "-5px",
+          fontWeight: "700",
+        },
+      ],
+      h2: [
+        "28px",
+        {
+          lineHeight: "34px",
+          fontWeight: "700",
+        },
+      ],
+      h3: [
+        "16px",
+        {
+          lineHeight: "19px",
+          letterSpacing: "15px",
+          fontWeight: "700",
+        },
+      ],
+      h4: [
+        "13px",
+        {
+          lineHeight: "16px",
+          letterSpacing: "5px",
+          fontWeight: "700",
+        },
+      ],
+      body1: [
+        "14px",
+        {
+          lineHeight: "18px",
+          fontWeight: "700",
+        },
+      ],
+      body2: [
+        "12px",
+        {
+          lineHeight: "14px",
+          fontWeight: "700",
+        },
+      ],
+    },
     screens: {
       xs: "375px",
       sm: "600px",
