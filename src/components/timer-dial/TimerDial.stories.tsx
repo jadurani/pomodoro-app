c1593405be6c03dfbf3dcbc5f4fa9430826a7353
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-03-24 14:07:21 +0800

    feat: created timer dial component

---
+++ src/components/timer-dial/TimerDial.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from "@storybook/react";
+import TimerDial from "./TimerDial";
+
+const meta: Meta<typeof TimerDial> = {
+  title: "Components/TimerDial",
+  component: TimerDial,
+  decorators: [
+    (Story) => (
+      <div className="fixed top-0 left-0 h-full w-full p-4">{Story()}</div>
+    ),
+  ],
+};
+
+export default meta;
+
+export const Timer: StoryObj<typeof TimerDial> = {
+  render: (args) => <TimerDial {...args} />,
+};
+
+Timer.args = {
+  id: "pomodoro",
+  timeDuration: 1500,
+  timeLeft: 900,
+};
---
+++ src/components/timer-dial/TimerDial.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from "react";
+import styles from "./TimerDial.module.css";
+
+export interface ITimerDial {
+  id: string;
+  /**
+   * Time left in seconds
+   */
+  timeLeft: number;
+  /**
+   * Full time duration in seconds
+   */
+  timeDuration: number;
+}
+
+/**
+ * How many seconds in a minute
+ */
+const ONE_MINUTE = 60;
+/**
+ * How many milliseconds in a second
+ */
+const ONE_SECOND = 1000;
+
+const secondsToMinutesString = (seconds: number) =>
+  parseInt(`${seconds}`).toString().padStart(2, "0");
+
+const TimerDial: React.FC<ITimerDial> = ({ id, timeLeft, timeDuration }) => {
+  const [timeRemaining, setTimeRemaining] = useState(timeLeft);
+  const [paused, setPaused] = useState(true);
+
+  const circleLength = (timeDuration - timeRemaining) / timeDuration;
+  const minutesTime = secondsToMinutesString(timeRemaining / ONE_MINUTE);
+  const secondsTime = secondsToMinutesString(timeRemaining % ONE_MINUTE);
+  const timeDisplay = `${minutesTime}:${secondsTime}`;
+  const isFinished = timeRemaining <= 0;
+
+  let buttonText = "Pause";
+  if (paused) {
+    buttonText = "Resume";
+  }
+  if (isFinished) {
+    buttonText = "Restart";
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (!paused && !isFinished) {
+        setTimeRemaining((t) => t - 1);
+      }
+    }, ONE_SECOND);
+
+    return () => clearInterval(intervalId);
+  }, [paused, isFinished]);
+
+  const handleToggle = () => {
+    setPaused(!paused);
+
+    if (isFinished) {
+      setTimeRemaining(timeDuration);
+      setPaused(false);
+    }
+  };
+
+  return (
+    <div className={styles.timer} id={id}>
+      <svg className="h-full w-full text-red">
+        <circle
+          cx="50%"
+          cy="50%"
+          r="150"
+          pathLength="1"
+          className={styles.countdown}
+          style={{
+            strokeDashoffset: circleLength,
+          }}></circle>
+        <text
+          x="50%"
+          y="50%"
+          textAnchor="middle"
+          className={styles.countdownText}>
+          {timeDisplay}
+        </text>
+      </svg>
+      <button
+        className="uppercase text-grey text-h3 absolute left-1/2 -translate-x-1/2 bottom-1/3 text-center"
+        onClick={() => handleToggle()}>
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+
+export default TimerDial;
