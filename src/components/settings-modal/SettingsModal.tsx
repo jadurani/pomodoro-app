a86304800cae7857e897d891b0ebf57269e93d43
Jadurani Davalos <jadurani.davalos@gmail.com>
2023-03-27 12:15:51 +0800

    fix: turn attributes to pascalCase

--- src/components/settings-modal/SettingsModal.tsx
+++ src/components/settings-modal/SettingsModal.tsx
@@ -81,8 +81,8 @@ const SettingsModal: React.FC<ISettingsModal> = ({ isOpen, setIsOpen }) => {
               xmlns="http://www.w3.org/2000/svg">
               <g opacity="0.5">
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M13.3639 2.05044L11.9497 0.63623L6.99995 5.58598L2.0502 0.63623L0.635986 2.05044L5.58573 7.00019L0.635986 11.9499L2.0502 13.3642L6.99995 8.4144L11.9497 13.3642L13.3639 11.9499L8.41416 7.00019L13.3639 2.05044Z"
                   fill="#1E213F"
                 />
